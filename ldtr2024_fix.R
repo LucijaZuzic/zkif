1045ae55836933c0343caacfd493742aea0e5668
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-09-12 06:24:49 +0200

    Update ldtr2024_fix.R

--- ldtr2024_fix.R
+++ ldtr2024_fix.R
@@ -6,7 +6,22 @@ rm(list=ls())
 #2. TEC data set, as derived from the RINEX GPS observations using 
 # GPS TEC software - enclosed
 
-setwd('C:/Users/RYZEN/Downloads/zkif-20240911T131920Z-001/zkif')
+library(tidyverse)
+getCurrentFileLocation <-  function()
+{
+    this_file <- commandArgs() %>% 
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+    if (length(this_file)==0)
+    {
+      this_file <- rstudioapi::getSourceEditorContext()$path
+    }
+    return(dirname(this_file))
+}
+
+setwd(getCurrentFileLocation())
 
 #Declaration of the R libraries utilised
 
